a7fef1b297bd7bfc5da03add514acf8b00d1072c
jasiumayen <igorpiszczatowski@gmail.com>
2025-03-02 18:59:15 +0100

    export function that sets up AP to separate file

--- main/main.cpp
+++ main/main.cpp
@@ -3,42 +3,7 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "Wifi.h"
-
-void setup_wifi_ap()
-{
-    const char *ssid = "tomek";
-    const char *password = "ptakilatajakluczem";
-
-    IPAddress local_IP(192, 168, 1, 22);
-    IPAddress gateway(192, 168, 1, 5);
-    IPAddress subnet(255, 255, 255, 0);
-
-    ESP_LOGI("AP_SETUP", "Setting up Access Point ... ");
-    bool is_ap_ok = WiFi.softAPConfig(local_IP, gateway, subnet);
-    if (is_ap_ok == true)
-    {
-        ESP_LOGI("AP_SETUP", "AP is OK!");
-    }
-    else
-    {
-        ESP_LOGE("AP_SETUP", "AP is shit!");
-    }
-
-    ESP_LOGI("AP_SETUP", "Starting Access Point ... ");
-    bool is_ap_working = WiFi.softAP(ssid, password);
-    if (is_ap_working == true)
-    {
-        ESP_LOGI("AP_SETUP", "AP is working!");
-    }
-    else
-    {
-        ESP_LOGE("AP_SETUP", "AP is not working!");
-    }
-
-    IPAddress ip = WiFi.softAPIP();
-
-    ESP_LOGI("AP_SETUP", "IP address: %s", ip.toString().c_str());
-}
+#include "wifi_setup.h"
 
 extern "C" void app_main()
 {
---
+++ main/wifi_setup.h
@@ -0,0 +1,6 @@
+#ifndef WIFI_AP_H
+#define WIFI_AP_H
+
+void setup_wifi_ap();
+
+#endif
