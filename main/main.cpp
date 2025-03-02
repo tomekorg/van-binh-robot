b8a4487e3ed497cd046bd7ae2a41d1373f3a5951
jasiumayen <igorpiszczatowski@gmail.com>
2025-03-02 18:33:10 +0100

    Added function that sets up AP to main

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,47 @@
 #include "Arduino.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "Wifi.h"
+
+void setup_wifi_ap()
+{
+    const char *ssid = "tomek";
+    const char *password = "ptakilatajakluczem";
+
+    IPAddress local_IP(192, 168, 1, 22);
+    IPAddress gateway(192, 168, 1, 5);
+    IPAddress subnet(255, 255, 255, 0);
+
+    ESP_LOGI("AP_SETUP", "Setting up Access Point ... ");
+    bool is_ap_ok = WiFi.softAPConfig(local_IP, gateway, subnet);
+    if (is_ap_ok == true)
+    {
+        ESP_LOGI("AP_SETUP", "AP is OK!");
+    }
+    else
+    {
+        ESP_LOGE("AP_SETUP", "AP is shit!");
+    }
+
+    ESP_LOGI("AP_SETUP", "Starting Access Point ... ");
+    bool is_ap_working = WiFi.softAP(ssid, password);
+    if (is_ap_working == true)
+    {
+        ESP_LOGI("AP_SETUP", "AP is working!");
+    }
+    else
+    {
+        ESP_LOGE("AP_SETUP", "AP is not working!");
+    }
+
+    IPAddress ip = WiFi.softAPIP();
+
+    ESP_LOGI("AP_SETUP", "IP address: %s", ip.toString().c_str());
+}
 
 extern "C" void app_main()
 {
     initArduino();
-    pinMode(4, OUTPUT);
-    digitalWrite(4, HIGH);
-    // Do your own thing
+    setup_wifi_ap();
 }
