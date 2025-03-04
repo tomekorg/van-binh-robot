5e17ff9285caa62c27e5a28c12a78ebf5c7647e3
olaputer <olasznajder05@gmail.com>
2025-03-04 21:00:06 +0100

    joystick with nipple.js

--- web-ui/index.js
+++ web-ui/index.js
@@ -1,5 +1,13 @@
-console.log('dziala:)');
+import nipplejs from 'nipplejs';
 
 document.getElementById("start").addEventListener("click", function() {
     console.log("klik");
 });
+
+let options = {
+    zone: document.getElementById('joystick-container'),
+    color: 'red',
+    mode: 'static',
+};
+
+let manager = nipplejs.create(options);
