b79eab40fbc4b902ae7798e8d0e53d053f91c91f
Bartlomiej Maciejiczek <bartlomiej.maciejiczek@nwboxed.com>
2025-03-05 16:22:26 +0100

    format with prettier

--- web-ui/index.js
+++ web-ui/index.js
@@ -1,13 +1,13 @@
-import nipplejs from 'nipplejs';
+import nipplejs from "nipplejs";
 
-document.getElementById("start").addEventListener("click", function() {
-    console.log("klik");
+document.getElementById("start").addEventListener("click", function () {
+  console.log("klik");
 });
 
 let options = {
-    zone: document.getElementById('joystick-container'),
-    color: 'red',
-    mode: 'static',
+  zone: document.getElementById("joystick"),
+  color: "red",
+  mode: "static",
 };
 
 let manager = nipplejs.create(options);
