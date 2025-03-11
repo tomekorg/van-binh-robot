911a80f32ea2a5ed6f3cd1104036f4c755407628
jasiumayen <igorpiszczatowski@gmail.com>
2025-03-11 18:38:03 +0100

    Add function that sets up HTTP server to main

--- main/main.cpp
+++ main/main.cpp
@@ -4,9 +4,30 @@
 #include "driver/gpio.h"
 #include "WiFi.h"
 #include "wifi_setup.h"
+#include <WebServer.h>
+
+WebServer server(80);
+
+void handleRoot()
+{
+    ESP_LOGI("WEBSERVER", "Received GET request for /");
+    server.send(200, "text/plain", "hello!");
+}
 
 extern "C" void app_main()
 {
     initArduino();
     setup_wifi_ap();
+
+    server.on("/", HTTP_GET, handleRoot);
+    ESP_LOGI("WEB_SERVER", "GET / handler registered")
+
+    server.begin();
+    ESP_LOGI("WEB_SERVER", "Web server started on port 80");
+
+    while (1)
+    {
+        server.handleClient();
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
 }
