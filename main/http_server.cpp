1241f0ace5224722d384b582532217486753d9ac
Bartlomiej Maciejiczek <bartlomiej.maciejiczek@nwboxed.com>
2025-03-13 22:12:09 +0100

    replace arduino WebServer with ESPAsyncWebServer

--- main/http_server.cpp
+++ main/http_server.cpp
@@ -1,26 +1,42 @@
-#include "Arduino.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "WiFi.h"
-#include <WebServer.h>
 #include "http_server.h"
+#include "Arduino.h"
+#include <ESPAsyncWebServer.h>
 
-WebServer server(80);
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
-void handleRoot()
-{
-    ESP_LOGI("WEBSERVER", "Received GET request for /");
-    server.send(200, "text/plain", "hello!");
+static const char *TAG = "HTTP/WS SERVER";
+
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+               AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+  case WS_EVT_CONNECT:
+    ESP_LOGI(TAG, "WebSocket client #%lu connected from %s", client->id(),
+             client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    ESP_LOGI(TAG, "WebSocket client #%lu disconnected", client->id());
+    break;
+  case WS_EVT_DATA:
+    ESP_LOGI(TAG, "WebSocket data received: %u bytes", len);
+    break;
+  default:
+    break;
+  }
 }
 
-WebServer& init_http_server()
-{
-    server.on("/", HTTP_GET, handleRoot);
-    ESP_LOGI("WEB_SERVER", "GET / handler registered");
+void handleRoot(AsyncWebServerRequest *request) {
+  ESP_LOGI("WEB_SERVER", "Received GET request on /");
+  request->send(200, "text/plain", "hello!");
+}
 
-    server.begin();
-    ESP_LOGI("WEB_SERVER", "Web server started on port 80");
+void init_http_server() {
+  server.on("/", HTTP_GET, handleRoot);
+  ESP_LOGI(TAG, "GET / handler registered");
 
-    return server;
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+
+  server.begin();
+  ESP_LOGI(TAG, "HTTP server started on port 80");
 }
